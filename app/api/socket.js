3e758b1d805d0166a78aead3beb09c60e975591b
winnyDiese <winnydiese@gmail.com>
2023-12-27 12:56:10 +0100

    All modifiacte

---
+++ app/api/socket.js
@@ -0,0 +1,17 @@
+
+import {Server} from "socket.io"
+
+export default function SocketHandler(req,res){
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on("connection", socket =>{
+        socket.on("send-message", obj =>{
+            io.emit("receive-message", obj)   
+        })
+    })
+
+    console.log("Setting Socket ")
+    res.end()
+
+}
